5b88dcaf10809fd44908e40cfd0a9799b933bc2f
macbook air <macbookair@macbooks-MacBook-Air-2.local>
2024-01-23 00:29:29 +0400

    added delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import data from'./data.js'
+import List from './List';
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
   )
 }
 
--- src/List.jsx
+++ src/List.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-function List() {
+function List({people}) {
     return (
         <>
+            {people.map((person) => {
+                const {id, name, age, image} = person;
+                return (
+                    <article key={id} className='person'>
+                        <img src={image} alt="" />
+                        <div>
+                            <h4>{name}</h4>
+                            <p>{age}</p>
+                        </div>
+                    </article>
+                );
+            })}
         </>
-    )
+    );
 }
 
-export default List
+export default List;
